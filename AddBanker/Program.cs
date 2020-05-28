e73846383a88722eb487ba6e1980204d0fc6f267
Никола Христозов <nikolahristozov02@mg-bg.eu>
2020-05-28 23:28:30 +0300

    AddBanker

---
+++ AddBanker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Net.Http.Headers;
+
+namespace Banking
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Name:");
+			string name=Console.ReadLine();
+			Console.Write("Userame:");
+			string username = Console.ReadLine();
+			Console.Write("Password:");
+			string password = Console.ReadLine();
+			SqlConnection sqlConnection = new SqlConnection("Data Source=localhost;Initial Catalog=bank;Integrated Security=True");
+			sqlConnection.Open();
+			SqlCommand sqlCommand = new SqlCommand("INSERT INTO bankers (username, password, name) VALUES ('"+username+ "', '" + password + "', '" + name + "');", sqlConnection);
+			sqlCommand.ExecuteNonQuery();
+			sqlConnection.Close();
+
+
+
+		}
+	}
+}
