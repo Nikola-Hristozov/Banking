c605186487670ce45fefafbff7d261ab8552d78c
Никола Христозов <nikolahristozov02@mg-bg.eu>
2020-05-30 11:08:59 +0300

    Changed database constructor Addedvalidation on AddBanker Project

--- AddBanker/Program.cs
+++ AddBanker/Program.cs
@@ -7,22 +7,57 @@ namespace Banking
 {
 	class Program
 	{
+		//Calls the AddBanker Method. If the query returns trows exception because the username is already in use the method is calleda again
 		static void Main(string[] args)
 		{
-			Console.Write("Name:");
-			string name=Console.ReadLine();
+			bool flag=false;
+			do
+			{
+				try
+				{
+					AddBanker();
+					Console.WriteLine("Succesfully added new banker account");
+					flag = false;
+				}
+				catch (Exception)
+				{
+					flag = true;
+					Console.WriteLine("Username is already in use.");
+				}
+			}
+			while (flag);
+
+		}
+		//Adds new banker account to the database
+		private static void AddBanker()
+		{
+			bool flag = false;
+			string name;
+			do
+			{
+				Console.Write("Name:");
+				name = Console.ReadLine();
+				foreach (var a in name)
+				{
+					if (char.IsDigit(a))
+					{
+						flag = true;
+					}
+				}
+				if(flag)
+				{
+					Console.WriteLine("Name cannot contain digits");
+				}
+			} while (flag);
 			Console.Write("Userame:");
 			string username = Console.ReadLine();
 			Console.Write("Password:");
 			string password = Console.ReadLine();
 			SqlConnection sqlConnection = new SqlConnection("Data Source=localhost;Initial Catalog=bank;Integrated Security=True");
 			sqlConnection.Open();
-			SqlCommand sqlCommand = new SqlCommand("INSERT INTO bankers (username, password, name) VALUES ('"+username+ "', '" + password + "', '" + name + "');", sqlConnection);
+			SqlCommand sqlCommand = new SqlCommand("INSERT INTO bankers (username, password, name) VALUES ('" + username + "', '" + password + "', '" + name + "');", sqlConnection);
 			sqlCommand.ExecuteNonQuery();
 			sqlConnection.Close();
-
-
-
 		}
 	}
 }
---
+++ DataBaseConstructor.sql
@@ -0,0 +1,37 @@
+﻿CREATE TABLE [dbo].[bankers] (
+    [id]       INT          IDENTITY (1, 1) NOT NULL,
+    [username] VARCHAR (50) NOT NULL,
+    [password] VARCHAR (50) NOT NULL,
+    [name]     VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC),
+    UNIQUE NONCLUSTERED ([username] ASC)
+);
+CREATE TABLE [dbo].[Creditors] (
+    [Id]        INT        IDENTITY (1, 1) NOT NULL,
+    [IBAN]      VARCHAR(50) NOT NULL,
+    [name]      VARCHAR(50) NOT NULL,
+    [lend]      FLOAT (53) NOT NULL,
+    [endDate]   DATE       NOT NULL,
+    [interest]  FLOAT (53) NOT NULL,
+    [guarantee] FLOAT (53) NOT NULL,
+    [banker_id] INT        NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([IBAN] ASC),
+    CONSTRAINT [FK_Creditors_Bankers] FOREIGN KEY ([banker_id]) REFERENCES [dbo].[bankers] ([id])
+);
+
+CREATE TABLE [dbo].[Debtors] (
+    [Id]          INT        IDENTITY (1, 1) NOT NULL,
+    [IBAN]        VARCHAR(50) NOT NULL,
+    [name]        VARCHAR(50) NOT NULL,
+    [debt]        FLOAT (53) NOT NULL,
+    [endDate]     DATE       NOT NULL,
+    [interest]    FLOAT (53) NOT NULL,
+    [installment] FLOAT (53) NOT NULL,
+    [banker_id]   INT        DEFAULT ((1)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([IBAN] ASC),
+    CONSTRAINT [FK_Debtors_Bankers] FOREIGN KEY ([banker_id]) REFERENCES [dbo].[bankers] ([id])
+);
+
+
